0ff2e272d95763888a64c0c927f947c76d02470b
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2025-07-22 14:37:18 +0530

    Upload files for project1

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🚀 Welcome to the React Admin Dashboard</h1>
+      <p>This is your live dashboard hosted on AWS S3 with CI/CD!</p>
+    </div>
+  );
+}
+
+export default App;
