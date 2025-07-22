c0c74ae120fbce01ef199c49fd9896e0b52fbaee
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2025-07-22 14:57:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>🚀 Welcome to the React Admin Dashboard</h1>
+      <h1>🚀 Welcome to the React Admin Dashboard Demo</h1>
       <p>This is your live dashboard hosted on AWS S3 with CI/CD!</p>
     </div>
   );
